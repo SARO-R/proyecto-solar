119fe8cb51c05143331099dcc8964a67a532343b
SARO-R <johanbuitrago339@gmail.com>
2026-03-25 23:09:09 -0500

    Replace saludar function with login and enviar functions

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-function saludar() {
-    alert("Hola, funciona correctamente 🚀");
+function login() {
+    let user = document.getElementById("usuario").value;
+    let pass = document.getElementById("password").value;
+
+    if (user === "" || pass === "") {
+        alert("Completa todos los campos");
+    } else {
+        window.location.href = "formulario.html";
+    }
 }
+
+function enviar() {
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+    let estado = document.getElementById("estado").value;
+
+    if (nombre === "" || edad === "" || estado === "") {
+        alert("Completa todos los campos");
+    } else {
+        localStorage.setItem("estado", estado);
+        window.location.href = "resultados.html";
+    }
+}
+
+window.onload = function() {
+    let resultado = document.getElementById("resultado");
+
+    if (resultado) {
+        let estado = localStorage.getItem("estado");
+
+        if (estado === "bien") {
+            resultado.innerHTML = "Resultado: Estado positivo 😊";
+        } else if (estado === "regular") {
+            resultado.innerHTML = "Resultado: Estado neutral 😐";
+        } else {
+            resultado.innerHTML = "Resultado: Estado negativo 😔";
+        }
+    }
+};
